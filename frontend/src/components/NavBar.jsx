3018d0fa7926028fdb33e660687146b99da76d0b
mikhail <mikhail.waddell@gmail.com>
2024-08-25 16:16:30 -0400

    bootstrap navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,47 +1,158 @@
-import {
-  Box,
-  Flex,
-  HStack,
-  IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Avatar,
-  Badge,
-  Drawer,
-  DrawerBody,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  useDisclosure,
-  Link,
-} from '@chakra-ui/react';
-import { HamburgerIcon, ChevronDownIcon } from '@chakra-ui/icons';
-import { FaUser } from 'react-icons/fa';
-import { ShoppingCart } from 'lucide-react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import SearchBar from './SearchBar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Offcanvas from 'react-bootstrap/Offcanvas';
+// import { LinkContainer } from 'react-router-bootstrap';
+// import { logout } from '../actions/userActions';
+// import logo from '../assets/images/bonsai-tree-logo.png';
+// import '../assets/styles/Header.css';
+// import { useNavigate, useLocation } from 'react-router-dom';
+// import { FaUser } from 'react-icons/fa6';
+// import { ShoppingCart } from 'lucide-react';
+// import { clearCart } from '../actions/cartActions';
+// const withoutSidebarRoutes = ['/login', '/register'];
+
+// function NavBar() {
+//   const { pathname } = useLocation();
+//   const userLogin = useSelector(state => state.userLogin);
+//   const { userInfo } = userLogin;
+//   const cart = useSelector(state => state.cart);
+//   const { cartItems } = cart;
+//   const navigate = useNavigate();
+
+//   const dispatch = useDispatch();
+
+//   const logoutHandler = () => {
+//     dispatch(logout());
+//     dispatch(clearCart());
+//     navigate('/');
+//   };
+
+//   const withoutSidebarRoutes = ['/login', '/register'];
+//   if (withoutSidebarRoutes.some(item => pathname.includes(item))) return null;
+
+//   return (
+//     <>
+//       <Navbar expand={'md'} className="navbar navbar-expand-lg navbar-dark ">
+//         <Container fluid id="nav">
+//           <LinkContainer to="/">
+//             <Navbar.Brand className="logo-container" id={'title-text'}>
+//               PLANTIS<img src={logo}></img>
+//             </Navbar.Brand>
+//           </LinkContainer>
+//           <Navbar.Toggle
+//             aria-controls={`offcanvasNavbar-expand-md`}
+//             id="menu-button"
+//           />
+//           <Navbar.Offcanvas
+//             id={`offcanvasNavbar-expand-md`}
+//             aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+//             placement="end"
+//           >
+//             <Offcanvas.Header closeButton>
+//               <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+//                 MENU
+//               </Offcanvas.Title>
+//             </Offcanvas.Header>
+//             <Offcanvas.Body>
+//               <SearchBar />
+
+//               <Nav className="justify-content-end flex-grow-1 pe-3 ">
+//                 <NavDropdown title="Shop" id="basic-nav-dropdown">
+//                   <LinkContainer to="/plants">
+//                     <NavDropdown.Item>Potted Plants</NavDropdown.Item>
+//                   </LinkContainer>
+//                   <LinkContainer to="/planters">
+//                     <NavDropdown.Item>Planters</NavDropdown.Item>
+//                   </LinkContainer>
+//                   <LinkContainer to="/essentials">
+//                     <NavDropdown.Item>Essentials</NavDropdown.Item>
+//                   </LinkContainer>
+//                 </NavDropdown>
+//                 <LinkContainer to="/cart">
+//                   <Nav.Link id={'cartLogo'}>
+//                     <ShoppingCart />{' '}
+//                     {`${cartItems.reduce((acc, item) => acc + item.qty, 0)}`}
+//                   </Nav.Link>
+//                 </LinkContainer>
+
+//                 {userInfo ? (
+//                   <NavDropdown
+//                     title={`Welcome ${' '} ${userInfo.name}`}
+//                     id="username"
+//                   >
+//                     <LinkContainer to="/profile">
+//                       <NavDropdown.Item>Profile</NavDropdown.Item>
+//                     </LinkContainer>
+//                     <NavDropdown.Item onClick={logoutHandler}>
+//                       Logout
+//                     </NavDropdown.Item>
+//                   </NavDropdown>
+//                 ) : (
+//                   <LinkContainer to="/login" className="d-flex">
+//                     <Nav.Link id={'login'}>
+//                       <FaUser />
+//                       Login
+//                     </Nav.Link>
+//                   </LinkContainer>
+//                 )}
+
+//                 {userInfo && userInfo.isAdmin && (
+//                   <NavDropdown title="Admin" id="adminmenue">
+//                     <LinkContainer to="/admin/userlist">
+//                       <NavDropdown.Item>Users</NavDropdown.Item>
+//                     </LinkContainer>
+
+//                     <LinkContainer to="/admin/productlist">
+//                       <NavDropdown.Item>Products</NavDropdown.Item>
+//                     </LinkContainer>
+
+//                     <LinkContainer to="/admin/orderlist">
+//                       <NavDropdown.Item>Orders</NavDropdown.Item>
+//                     </LinkContainer>
+//                   </NavDropdown>
+//                 )}
+//               </Nav>
+//             </Offcanvas.Body>
+//           </Navbar.Offcanvas>
+//         </Container>
+//       </Navbar>
+//     </>
+//   );
+// }
+
+// export default NavBar;
+
 import { useDispatch, useSelector } from 'react-redux';
-import { Link as RouterLink, useNavigate, useLocation } from 'react-router-dom';
-import { logout } from '../actions/userActions';
-import { clearCart } from '../actions/cartActions';
-import logo from '../assets/images/bonsai-tree-logo.png';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import SearchBar from './SearchBar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { LinkContainer } from 'react-router-bootstrap';
+import { logout } from '../actions/userActions';
+import logo from '../assets/images/bonsai-tree-logo.png';
+import '../assets/styles/Header.css';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa6';
+import { ShoppingCart } from 'lucide-react';
+import { clearCart } from '../actions/cartActions';
 
 const withoutSidebarRoutes = ['/login', '/register'];
 
 function NavBar() {
   const { pathname } = useLocation();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
   const userLogin = useSelector(state => state.userLogin);
   const { userInfo } = userLogin;
-
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart;
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
 
   const logoutHandler = () => {
     dispatch(logout());
@@ -49,243 +160,96 @@ function NavBar() {
     navigate('/');
   };
 
+  const withoutSidebarRoutes = ['/login', '/register'];
   if (withoutSidebarRoutes.some(item => pathname.includes(item))) return null;
 
   return (
-    <Box
-      bg="transparent"
-      px={4}
-      position="fixed"
-      top={0}
-      width="100%"
-      zIndex={10}
-    >
-      <Flex h={16} alignItems="center" justifyContent="space-between">
-        {/* Left Section: Logo and Navigation Links */}
-        <HStack spacing={8} alignItems="center">
-          <RouterLink to="/">
-            <HStack>
-              <Box fontSize="xl" fontWeight="bold" color="#304732">
-                BONSAI
-              </Box>
-              <img src={logo} alt="Bonsai logo" style={{ height: '40px' }} />
-            </HStack>
-          </RouterLink>
+    <>
+      <Navbar expand={'lg'} className="transparent-navbar navbar-dark">
+        <Container fluid id="nav">
+          <LinkContainer to="/">
+            <Navbar.Brand className="logo-container" id={'title-text'}>
+              PLANTIS<img src={logo} alt="logo"></img>
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-expand-md`}
+            id="menu-button"
+          />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-md`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+                MENU
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <SearchBar />
 
-          {/* Shop Menu Button */}
-          <Menu>
-            <MenuButton
-              as={Button}
-              rightIcon={<ChevronDownIcon />}
-              variant="gholst"
-              fontWeight="bold"
-              color="#304732"
-              _hover={{
-                bg: 'transparent',
-                color: '#48bb78',
-              }}
-              _active={{
-                bg: 'transparent',
-                transform: 'scale(0.95)',
-              }}
-            >
-              Shop
-            </MenuButton>
-            <MenuList boxShadow="lg" borderRadius="md">
-              <MenuItem as={RouterLink} to="/plants">
-                Potted Plants
-              </MenuItem>
-              <MenuItem as={RouterLink} to="/planters">
-                Planters
-              </MenuItem>
-              <MenuItem as={RouterLink} to="/essentials">
-                Essentials
-              </MenuItem>
-            </MenuList>
-          </Menu>
+              <Nav className="justify-content-end flex-grow-1 pe-3 ">
+                <NavDropdown title="Shop" id="basic-nav-dropdown">
+                  <LinkContainer to="/plants">
+                    <NavDropdown.Item>Potted Plants</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/planters">
+                    <NavDropdown.Item>Planters</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/essentials">
+                    <NavDropdown.Item>Essentials</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+                <LinkContainer to="/cart">
+                  <Nav.Link id={'cartLogo'}>
+                    <ShoppingCart />{' '}
+                    {`${cartItems.reduce((acc, item) => acc + item.qty, 0)}`}
+                  </Nav.Link>
+                </LinkContainer>
 
-          {/* Blog and About Links */}
-          <RouterLink to="/blog">
-            <Button
-              variant="link"
-              color="#304732"
-              _hover={{ color: '#48bb78' }}
-            >
-              Blog
-            </Button>
-          </RouterLink>
-          <RouterLink to="/about">
-            <Button
-              variant="link"
-              color="#304732"
-              _hover={{ color: '#48bb78' }}
-            >
-              About
-            </Button>
-          </RouterLink>
-        </HStack>
+                {userInfo ? (
+                  <NavDropdown
+                    title={`Welcome ${' '} ${userInfo.name}`}
+                    id="username"
+                  >
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <LinkContainer to="/login" className="d-flex">
+                    <Nav.Link id={'login'}>
+                      <FaUser />
+                      Login
+                    </Nav.Link>
+                  </LinkContainer>
+                )}
 
-        {/* Right Section: Profile, Cart, Login/Logout */}
-        <Flex alignItems="center">
-          <RouterLink to="/cart">
-            <Box position="relative">
-              <IconButton
-                aria-label="Shopping Cart"
-                icon={<ShoppingCart />}
-                size="lg"
-                variant="link"
-                color="#304732"
-                _hover={{
-                  bg: 'transparent',
-                  transform: 'scale(1.05)',
-                }}
-                _active={{
-                  bg: 'transparent',
-                  transform: 'scale(0.95)',
-                }}
-                mr={4}
-              />
-              {cartItems.length > 0 && (
-                <Badge
-                  colorScheme="green"
-                  borderRadius="full"
-                  position="absolute"
-                  top="0"
-                  right="6"
-                  transform="translate(50%, -50%)"
-                  fontSize="0.8em"
-                >
-                  {cartItems.reduce((acc, item) => acc + item.qty, 0)}
-                </Badge>
-              )}
-            </Box>
-          </RouterLink>
+                {userInfo && userInfo.isAdmin && (
+                  <NavDropdown title="Admin" id="adminmenue">
+                    <LinkContainer to="/admin/userlist">
+                      <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
 
-          {userInfo ? (
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded="full"
-                variant="link"
-                cursor="pointer"
-                minW={0}
-              >
-                <Avatar size="sm" />
-              </MenuButton>
-              <MenuList>
-                <MenuItem as={RouterLink} to="/profile">
-                  Profile
-                </MenuItem>
-                <MenuItem onClick={logoutHandler}>Logout</MenuItem>
-              </MenuList>
-            </Menu>
-          ) : (
-            <Button
-              as={RouterLink}
-              to="/login"
-              leftIcon={<FaUser />}
-              variant="solid"
-              colorScheme="teal"
-              bgGradient="linear(to-r, teal.500, green.500)"
-              color="white"
-              borderRadius="full"
-              boxShadow="lg"
-              _hover={{
-                bgGradient: 'linear(to-r, teal.600, green.600)',
-                transform: 'scale(1.05)',
-              }}
-              _active={{
-                bgGradient: 'linear(to-r, teal.700, green.700)',
-                transform: 'scale(0.95)',
-              }}
-              ml={4}
-            >
-              Login
-            </Button>
-          )}
-        </Flex>
-      </Flex>
+                    <LinkContainer to="/admin/productlist">
+                      <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
 
-      {/* Mobile Drawer */}
-      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>MENU</DrawerHeader>
-          <DrawerBody>
-            <SearchBar />
-            <HStack
-              as="nav"
-              spacing={4}
-              flexDirection="column"
-              alignItems="start"
-            >
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rightIcon={<ChevronDownIcon />}
-                  bgGradient="linear(to-r, teal.500, green.500)"
-                  color="white"
-                  borderRadius="full"
-                  boxShadow="lg"
-                  _hover={{
-                    bgGradient: 'linear(to-r, teal.600, green.600)',
-                    transform: 'scale(1.05)',
-                  }}
-                  _active={{
-                    bgGradient: 'linear(to-r, teal.700, green.700)',
-                    transform: 'scale(0.95)',
-                  }}
-                >
-                  Shop
-                </MenuButton>
-                <MenuList boxShadow="lg" borderRadius="md">
-                  <MenuItem as={RouterLink} to="/plants">
-                    Potted Plants
-                  </MenuItem>
-                  <MenuItem as={RouterLink} to="/planters">
-                    Planters
-                  </MenuItem>
-                  <MenuItem as={RouterLink} to="/essentials">
-                    Essentials
-                  </MenuItem>
-                </MenuList>
-              </Menu>
-
-              <RouterLink to="/cart" onClick={onClose}>
-                <Flex alignItems="center">
-                  <ShoppingCart />
-                  {cartItems.length > 0 && (
-                    <Badge ml={2} colorScheme="red">
-                      {cartItems.reduce((acc, item) => acc + item.qty, 0)}
-                    </Badge>
-                  )}
-                </Flex>
-              </RouterLink>
-
-              {userInfo ? (
-                <>
-                  <MenuItem as={RouterLink} to="/profile" onClick={onClose}>
-                    Profile
-                  </MenuItem>
-                  <MenuItem onClick={logoutHandler}>Logout</MenuItem>
-                </>
-              ) : (
-                <Button
-                  as={RouterLink}
-                  to="/login"
-                  variant="outline"
-                  colorScheme="teal"
-                  mt={4}
-                >
-                  <FaUser /> Login
-                </Button>
-              )}
-            </HStack>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </Box>
+                    <LinkContainer to="/admin/orderlist">
+                      <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+                  </NavDropdown>
+                )}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
