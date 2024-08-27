7a36376cadf43c071f07f41d176ae5a6942a2592
mikhail <mikhail.waddell@gmail.com>
2024-08-27 16:22:51 -0400

    update register page

--- backend/base/models.py
+++ backend/base/models.py
@@ -4,6 +4,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class SiteUsers(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(
+        default="avatar.png", upload_to="profile_pictures"
+    )
+
+
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
