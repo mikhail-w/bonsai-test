6250d8705acc344b92309c37255a49141b5c0d11
Mourad Mourad <mourad272@icloud.com>
2024-08-29 13:05:10 -0500

    BlogPostListView created

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.pagination import PageNumberPagination
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10  
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class BlogPostListView(generics.ListAPIView):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    pagination_class = StandardResultsSetPagination
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
