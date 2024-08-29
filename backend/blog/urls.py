a75c9ed6d492a6a1b5cae25317cc29628acefe86
Mourad Mourad <mourad272@icloud.com>
2024-08-29 13:07:24 -0500

    first end point created 'blog'

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-# from django.views.generic import TemplateView
+from .views import BlogPostListView
 
 urlpatterns = [
-    path("", ),
+    path('blog/', BlogPostListView.as_view(), name='blog-list'),
 ]
