f6b594edac1744f497937bb850996824ceaec44c
Mourad Mourad <mourad272@icloud.com>
2024-08-29 19:03:31 -0500

    PostDetailView

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CreatePostView, BlogPostListView
+from .views import CreatePostView, BlogPostListView, PostDetailView
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='blog-list'),
     path('create/', CreatePostView.as_view(), name='create-post'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
+
+
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -48,3 +48,13 @@ class CreatePostView(generics.CreateAPIView):
         )
 
 
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.views += 1  
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
