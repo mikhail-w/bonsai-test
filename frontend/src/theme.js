abe6709f45b6ef8f30fcd75ce2da7ffb433fb61d
mikhail <mikhail.waddell@gmail.com>
2024-10-06 09:25:36 -0400

    update

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  config: {
+    initialColorMode: 'light', // Set default to light mode
+    useSystemColorMode: false,
+  },
   fonts: {
     body: "'Roboto', sans-serif", // Default for body text
     heading: "'Poppins', sans-serif", // Default for headings
@@ -13,6 +17,14 @@ const theme = extendTheme({
       light: '#7ed56f',
       medium: '#55c57a',
       dark: '#28b485',
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'white', // Ensure that the background is white
+        color: 'black',
+      },
     },
   },
 });
