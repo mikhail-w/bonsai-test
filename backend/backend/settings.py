1de58ea5d201af86a154e6788b0c16a45ad0f13f
mikhail <mikhail.waddell@gmail.com>
2024-10-05 21:56:16 -0400

    set up aws

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -186,6 +186,7 @@ STATIC_URL = "static/"
 MEDIA_URL = "/images/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA_ROOT = BASE_DIR / "static/images"
 MEDIA_ROOT = "static/images"
