26b510a28b8f4ad6b0e7ad3c99da32ca152f0695
mikhail <mikhail.waddell@gmail.com>
2024-12-09 17:05:34 -0500

    fix order view

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -6,6 +6,10 @@ from django.conf import settings
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product, Order, OrderItem, ShippingAddress, Review
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -114,14 +118,19 @@ class OrderSerializer(serializers.ModelSerializer):
 
     def get_orderItems(self, obj):
         items = obj.orderitem_set.all()
+        # logging.info(f"Order items for {obj}: {items}")
         serializer = OrderItemSerializer(items, many=True)
+        # logging.info(f"Serialized order items Count: {len(serializer.data)}\n")
         return serializer.data
 
     def get_shippingAddress(self, obj):
         try:
             address = ShippingAddressSerializer(obj.shippingaddress, many=False).data
-        except:
-            address = {}
+            # logging.info(f"Shipping address for {obj}: {address}")
+        except Exception as e:
+            # logging.error(f"Error fetching shipping address: {e}")
+            address = False
+            raise
         return address
 
     def get_user(self, obj):
