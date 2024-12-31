89e5d84b1103480917b360f4f5a0a2bf59f24c08
mikhail <mikhail.waddell@gmail.com>
2024-12-31 14:33:46 -0500

    updaate feature section

--- frontend/src/components/FeaturedProductsSection.jsx
+++ frontend/src/components/FeaturedProductsSection.jsx
@@ -1,47 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   SimpleGrid,
   Heading,
   Button,
-  useColorModeValue,
   Text,
+  Image,
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import p3 from '../assets/images/h10.jpg';
 import p4 from '../assets/images/potters.jpg';
 import p5 from '../assets/images/can.jpg';
+import '../assets/styles/card.css';
 
 const FeaturedProductsSection = () => {
-  const bg = useColorModeValue('white', 'gray.800');
-  const textColor = useColorModeValue('gray.700', 'white');
-
   const products = [
     {
-      title: 'Shop Plants',
+      title: 'Shop   Plants',
+      description: 'Discover a wide variety of plants.',
+      price: '$297',
       image: p3,
       path: '/plants',
+      gradient:
+        'linear-gradient(to bottom right, rgba(255, 159, 64, 0.8), rgba(255, 206, 0, 0.8))',
     },
     {
       title: 'Shop Planters',
+      description: 'Explore elegant planters for your plants.',
+      price: '$497',
       image: p4,
       path: '/planters',
+      gradient:
+        'linear-gradient(to bottom right, rgba(72, 239, 128, 0.8), rgba(72, 191, 145, 0.8))',
     },
     {
       title: 'Shop Accessories',
+      description: 'Find perfect accessories for bonsai care.',
+      price: '$897',
       image: p5,
       path: '/essentials',
+      gradient:
+        'linear-gradient(to bottom right, rgba(72, 145, 239, 0.8), rgba(72, 191, 255, 0.8))',
     },
   ];
 
+  const [flipped, setFlipped] = useState(Array(products.length).fill(false));
+
+  const handleMouseEnter = index => {
+    const newFlipped = [...flipped];
+    newFlipped[index] = true;
+    setFlipped(newFlipped);
+  };
+
+  const handleMouseLeave = index => {
+    const newFlipped = [...flipped];
+    newFlipped[index] = false;
+    setFlipped(newFlipped);
+  };
+
   return (
-    <Box mt={100} mb={100} py={16} textAlign="center" bg={bg} minH="100vh">
+    <Box mt={100} mb={100} py={16} textAlign="center" bg="white" minH="100vh">
       <Heading
-        fontFamily="lato"
+        fontFamily="montserrat, sans-serif"
         as="h2"
         size="2xl"
         mb={12}
-        paddingBottom={'50px'}
+        paddingBottom="50px"
         fontWeight="300"
         color="green.600"
       >
@@ -56,54 +80,59 @@ const FeaturedProductsSection = () => {
         {products.map((product, index) => (
           <Box
             key={index}
-            bgGradient="linear(to-b, green.50, beige.200)"
-            borderRadius="lg"
-            overflow="hidden"
-            boxShadow="lg"
-            position="relative"
-            transition="all 0.3s ease-in-out"
-            _hover={{
-              transform: 'scale(1.05)',
-              cursor: 'pointer',
-              bgGradient: 'linear(to-b, green.100, beige.300)',
-            }}
-            width="100%"
-            maxW="350px"
-            as={RouterLink}
-            to={product.path}
+            className="card"
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            cursor={'pointer'}
           >
-            {/* Background Image */}
             <Box
-              height="250px"
-              bgImage={`url(${product.image})`}
-              bgSize="cover"
-              bgPos="center"
-              position="relative"
-              borderBottom="2px solid green.300"
-              filter="brightness(0.85)"
-              transition="all 0.3s ease-in-out"
-              _hover={{ filter: 'brightness(1)', cursor: 'pointer' }}
-            />
-
-            {/* Card Content */}
-            <Box p={6}>
-              <Heading
-                fontFamily="'Caveat', cursive"
-                as="h3"
-                size="lg"
-                mb={4}
-                color="green.800"
-              >
-                {product.title}
-              </Heading>
-              <Text fontSize="sm" color="gray.500">
-                Discover the essence of bonsai beauty.
-              </Text>
+              className={`card__inner ${flipped[index] ? 'is-flipped' : ''}`}
+            >
+              {/* Front Side */}
+              <Box className="card__face card__face--front ">
+                <Image
+                  src={product.image}
+                  alt={product.title}
+                  className="card__image"
+                  borderRadius="lg"
+                  objectFit="cover"
+                />
+                <h4 className={`card__heading card__heading--${index + 1}`}>
+                  <span
+                    className={`card__heading-span card__heading-span--${
+                      index + 1
+                    }`}
+                  >
+                    {product.title}
+                  </span>
+                </h4>
+              </Box>
+              {/* Back Side */}
+              <Box className="card__face card__face--back">
+                <Box className="card__content">
+                  <Text fontSize="md" mb={4} color="white">
+                    {product.description}
+                  </Text>
+                  <Text fontSize="2xl" fontWeight="bold" mb={4} color="white">
+                    {product.price}
+                  </Text>
+                  <Button
+                    as={RouterLink}
+                    to={product.path}
+                    bg="green.500"
+                    color="white"
+                    _hover={{ bg: 'green.400' }}
+                    size="lg"
+                    borderRadius="full"
+                  >
+                    Shop Now
+                  </Button>
+                </Box>
+              </Box>
             </Box>
           </Box>
         ))}
       </SimpleGrid>
-
       <Button
         as={RouterLink}
         to="/products"
